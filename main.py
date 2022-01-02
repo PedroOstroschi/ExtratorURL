dafa57443247e3a6319c0a87c688769096a198cf
Pedro Ostrschi <pedrotangerino@hotmail.com>
2022-01-02 14:00:42 -0300

    Aula 1 introdução ao fatiamento de strings

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+url = "https://bytebank.com/cambio?moedaOrigem=real"
+# http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+print(url_base)
+url_parametros = url[indice_interrogacao + 1:]
+print(url_parametros)
